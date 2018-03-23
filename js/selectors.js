263d376fd36d90dabcf6f37b6ef289ecbe36b886
Dalton Pierce <dnnp2011@gmail.com>
2018-03-23 15:12:29 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -7,7 +7,11 @@ function lastImageSelector(){
 }
 
 function ninjaBabySelector(){
+<<<<<<< HEAD
   return $('#baby-ninja');
+=======
+  return $('#ninja-baby');
+>>>>>>> 891889e2b9d09b6095b059dca95e953ba6ad65ef
 }
 
 function divSelector(){
@@ -15,5 +19,9 @@ function divSelector(){
 }
 
 function firstListItem(){
+<<<<<<< HEAD
   return $('#pic-list li:first-child');
+=======
+  return $('ul#pic-list li');
+>>>>>>> 891889e2b9d09b6095b059dca95e953ba6ad65ef
 }
